e7e0ac9d45e0520f588b4a37e4908aeb6b0d34d7
renansfs <renansfs@gmail.com>
2018-09-25 23:15:05 -0300

    Genetic Algorithm

--- Genetic Algorithm/Algorithm.py
+++ Genetic Algorithm/Algorithm.py
@@ -1,9 +1,42 @@
 import pandas as pd
 import numpy as np
+import math as mt
+import operator as op
 
-def genetico(dataFrame, n_generation, mutation_rate):
-    pass
+def getBestGeneration(generations):
+    return max(generations.items(), key=op.itemgetter(1))[0]
 
+
+#Chamada do Algoritmo
+#df: dataframe
+#n_gene: maxima numero de geracoes
+#mutation_rate: taxa de mutacao
+#crossover_rate: taxa de crossover
+#features name
+#Retonar a melhor geracao
+def genetico(df: pd.DataFrame, max_genoma_size, mutation_rate, crossover_rate, max_generation_times, features, class_name):
+    generations = dict()
+    genome = list()
+    
+    for _ in range(max_genoma_size):
+        index = features[np.random.randint(len(features))]
+        genome.append(index)
+
+    for _ in range(max_generation_times):
+        evaluate = df.loc[:, genome]
+        variance = variances(evaluate)
+        cross = crossover(crossover_rate, genome, variance, features)
+        mutacao(df, mutation_rate, cross, features)
+        generation = pd.concat([evaluate, df[[class_name]]], axis=1)
+        measure = fitness(generation, class_name=class_name, target_feature_number=max_genoma_size)
+        generations[measure] = cross
+    return generations
+
+#Mutacao
+#df: Datafrma
+#mut_rate: taxa da mutacao maxima
+#genoma: features do genoma
+#features: features total
 def mutacao(df: pd.DataFrame, mut_rate, genome, features):
     prob = np.random.randint(100)
     if mut_rate > prob:
@@ -11,9 +44,14 @@ def mutacao(df: pd.DataFrame, mut_rate, genome, features):
         indexF = np.random.randint(len(features))
         genome[indexG] = features[indexF]
 
+
+#Taxa randomica do crossover
+#Crossover_rate: taxa maxima para acontecer o crossover
+#genome: features do genoma
+#variances: lista com variancias
+#features: features total
 def crossover(crossover_rate, genome, variances, features):
-    #Taxa randomica do crossover
-    random = np.random.rand() * np.random.randint(100) 
+    random = np.random.rand() * crossover_rate
     cross = []
     for feature in variances:
         if random < variances[feature]:
@@ -22,29 +60,35 @@ def crossover(crossover_rate, genome, variances, features):
         else:
             cross.append(feature)
     return cross
-    
+
+#Variancias
+#df: DataFrame
+#retorna o dicionario de variancias
 def variances(df: pd.DataFrame):
-    variance = pd.DataFrame.var().to_dict()
+    variance = df.var().to_dict()
     return variance
 
-def fitness(df: pd.DataFrame, class_name, target_feature_number, function, best_measurement, maximize_measurement=True):
-    # separa os grupos de classe 0 e 1
-    group_0 = df[df[class_name] == 0]
-    group_1 = df[df[class_name] == 1]
+def fitness(df: pd.DataFrame, class_name, target_feature_number):
+    try:
+        # separa os grupos de classe 0 e 1
+        group_0 = df[df[class_name] == 0]
+        group_1 = df[df[class_name] == 1]
 
-    # retira os campos classe para nao computar distancia entre eles
-    group_0 = group_0.drop(class_name, axis=1)
-    group_1 = group_1.drop(class_name, axis=1)
+        # retira os campos classe para nao computar distancia entre eles
+        group_0 = group_0.drop(class_name, axis=1)
+        group_1 = group_1.drop(class_name, axis=1)
 
-    # centroide as medias de cada feature
-    centroid_0 = []
-    for i in group_0:
-        centroid_0.append(group_0[i].mean())
-    centroid_1 = []
-    for i in group_1:
-        centroid_1.append(group_1[i].mean())
+        # centroide as medias de cada feature
+        centroid_0 = []
+        for i in group_0:
+            centroid_0.append(group_0[i].mean())
+        centroid_1 = []
+        for i in group_1:
+            centroid_1.append(group_1[i].mean())
 
-    distance = euclidian_distance(centroid_0, centroid_1)
+        distance = euclidian_distance(centroid_0, centroid_1)
+    except:
+        distance = 0
     return distance
 
 def euclidian_distance(arr1, arr2):
@@ -54,4 +98,4 @@ def euclidian_distance(arr1, arr2):
     distance = 0
     for i in range(0, len(arr1)):
         distance += (arr1[i] - arr2[i])**2
-    return math.sqrt(distance)
+    return mt.sqrt(distance)
--- Genetic Algorithm/main.py
+++ Genetic Algorithm/main.py
@@ -1,11 +1,11 @@
 import Algorithm
 import pandas as pd
 
+features = ['class','node-caps','deg-malig','breast','irradiat','age_20-29','age_30-39','age_40-49','age_50-59','age_60-69','age_70-79','menopause_ge40','menopause_lt40','menopause_premeno','tumor-size_0-4','tumor-size_10-14','tumor-size_15-19','tumor-size_20-24','tumor-size_25-29','tumor-size_30-34','tumor-size_35-39','tumor-size_40-44','tumor-size_45-49','tumor-size_5-9','tumor-size_50-54','inv-nodes_0-2','inv-nodes_12-14','inv-nodes_15-17','inv-nodes_24-26','inv-nodes_3-5','inv-nodes_6-8','inv-nodes_9-11','breast-quad_central','breast-quad_left_low','breast-quad_left_up','breast-quad_right_low','breast-quad_right_up']
+
 if __name__ == "__main__":
     filename = 'breast_cancer_final.csv'
-    breast_cancer_dataset = pd.read_csv(filename)
-    b_dict = breast_cancer_dataset.var().to_dict()
-    #f = ["A", "B","C","D","E","F"]
-    #g = ["E", "D","C","B","A"]
-    #d = { "E": 12, "D":25, "C": 75, "B": 30, "A":55}
-    #Algorithm.crossover(crossover_rate=100, genome=g, variances=d, features = f)
+    breast_cancer_dataset = pd.read_csv(filename, names=features)
+    result = Algorithm.genetico(breast_cancer_dataset, max_genoma_size = 5, mutation_rate=0.03, crossover_rate=0.5, max_generation_times=100, features=features, class_name='class')
+    value = Algorithm.getBestGeneration(result)
+    print(("Valor: %s Set: %s") % (value, result[value]))
